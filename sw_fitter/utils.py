4e1957d4d9b71eb6129df8251a932b7bdc90d9fd
abiciunas <alex.biciunas1@gmail.com>
2026-06-23 12:03:19 -0400

    Update utils.py

--- sw_fitter/utils.py
+++ sw_fitter/utils.py
@@ -1,16 +1,18 @@
-import torch
-from ase.units import Hartree, Bohr
+from ase.units import Bohr, Hartree
 
-HARTREE_TO_EV = Hartree
-BOHR_TO_ANGSTROM = Bohr
+HARTREE_TO_EV = Hartree  # multiply a Hartree energy to get eV
+BOHR_TO_ANGSTROM = Bohr  # multiply a Bohr length to get Angstrom
 EV_TO_HARTREE = 1.0 / Hartree
 ANGSTROM_TO_BOHR = 1.0 / Bohr
-FORCE_AU_TO_EV_ANG = Hartree / Bohr
+FORCE_AU_TO_EV_ANG = Hartree / Bohr  # Hartree/Bohr -> eV/Angstrom
 
 
-def inverse_softplus(x):
-    return torch.log(torch.expm1(torch.as_tensor(x, dtype=torch.float64)))
+def canonical_pair(type_a, type_b):
+    if type_a <= type_b:
+        return f"{type_a}-{type_b}"
+    return f"{type_b}-{type_a}"
 
 
-def canonical_pair(a, b):
-    return f"{a}-{b}" if a <= b else f"{b}-{a}"
+def canonical_triplet(centre, neighbour_a, neighbour_b):
+    first, second = sorted([neighbour_a, neighbour_b])
+    return f"{centre}:{first}-{second}"
